2b8835c25f65b13127168c968afb9f672eda7c09
stinkybootsllc <stinkybootsllc@gmail.com>
2019-01-12 16:56:54 -0500

    added owner db credentials file

---
+++ maintainers/includes/dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+	/*  
+	Date: 12 January 2019
+	Author: Eduardo Estrada
+	Title: dbconnect.php
+	Description: Database connection variables
+	*/
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'tenant_owner'); //tenant_user
+   define('DB_PASSWORD', 'owner4TenantPortal'); // 
+   define('DB_DATABASE', 'TenantPortal');
+
+   $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+    /* check connection */
+	if (!$conn) {
+		die("There has been an error -18");
+	} 
+
+?>
+
