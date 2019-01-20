bd5c97548b6753e07d7928384129e34fa6fe6879
stinkybootsllc <stinkybootsllc@gmail.com>
2019-01-20 17:08:09 -0500

    added footer

--- maintRequests/maintClosedIssues.php
+++ maintRequests/maintClosedIssues.php
@@ -4,14 +4,21 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
-    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  
+    <!-- Font-Awsome CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" 
+    integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" 
+    crossorigin="anonymous">
     <title>Tenant Portal 2019</title>
+    <meta name="author" content="Eduardo Estrada">
+    <meta name="description" content="description here">
+    <meta name="keywords" content="keywords,here">
+    <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+    <!-- CSS home made sources-->
+    <link rel="stylesheet" href="../assets/css/tenants.css">
+    <link rel="stylesheet" href="../assets/css/Footer-Dark.css">
+
   </head>
   <body>
 
@@ -126,6 +133,42 @@
             mysqli_close($conn);
         ?>
     </div>
+
+    <!--==== FOOTER  Bootstrap 4 Class  Footer-Dark.css =======-->
+    <div class="footer-dark">
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-3 item">
+                    <h2>Other Links</h2>
+                    <ul>
+                        <li><a href="#" alt="">link</a></li>
+                        <li><a href="#" alt="">link</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-3 item">
+                    <h2>About</h2>
+                    <ul>
+                        <li><a href="#">Purpose</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 item text">
+                    <h2>Main Site Title</h2>
+                    <p> sub paragraph</p>
+                    <img src="assets/img/logo.png" alt="" width="155">
+                </div>
+                <!-- Bottom Footer Icons -->
+                <div class="col item social"> 
+                    <a href="#"><i class="fab fa-facebook-square"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                </div>	
+            </div> <!-- end row -->
+            <p class="copyright">StinkyBoots Studio 2018</p>
+        </div>
+        </footer>
+    </div> <!-- end <div class="footer-dark">-->
+    <!--===== END FOOTER =============-->
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
--- maintainers/maintDash.php
+++ maintainers/maintDash.php
@@ -4,12 +4,20 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  
+    <!-- Font-Awsome CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" 
+    integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" 
+    crossorigin="anonymous">
     <title>Tenant Portal 2019</title>
+    <meta name="author" content="Eduardo Estrada">
+    <meta name="description" content="description here">
+    <meta name="keywords" content="keywords,here">
+    <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+    <!-- CSS home made sources-->
+    <link rel="stylesheet" href="../assets/css/tenants.css">
+    <link rel="stylesheet" href="../assets/css/Footer-Dark.css">
   </head>
   <body>
 
@@ -44,49 +52,70 @@
         <p>Maintenace Dashboard</p>
     </div> 
     <div class="container">
-    <div class="shadow p-3 mb-5 bg-white rounded">
-    <div class="card w-100">
-        <div class="card-body">
-        <h5 class="card-title">Open Maint Issues</h5>
-        <p class="card-text">Recently added tenant maintenance issues.</p>
-        <a href="../maintRequests/maintOpenIssues.php" class="btn btn-primary">Button</a>
-        </div>
-        </div>
-    
-    
-    
-    
-
-    
-    </div>
-    <div class="shadow p-3 mb-5 bg-white rounded">
-    <div class="card w-100">
-        <div class="card-body">
-        <h5 class="card-title">Pending Maint Issues</h5>
-        <p class="card-text">Pending issues with scheduled repair dates.</p>
-        <a href="../maintRequests/maintPendingIssues.php" class="btn btn-primary">Button</a>
-        </div>
+        <div class="shadow p-3 mb-5 bg-white rounded">
+            <div class="card w-100">
+                <div class="card-body">
+                    <h5 class="card-title">Open Maint Issues</h5>
+                    <p class="card-text">Recently added tenant maintenance issues.</p>
+                    <a href="../maintRequests/maintOpenIssues.php" class="btn btn-primary">Button</a>
+                </div> 
+            </div>
         </div>
    
-
-
-
-    </div>
-    <div class="shadow p-3 mb-5 bg-white rounded">
-
-        <div class="card w-100">
-        <div class="card-body">
-        <h5 class="card-title">Closed Maint Issues</h5>
-        <p class="card-text">Closed issue history.</p>
-        <a href="../maintRequests/maintClosedIssues.php" class="btn btn-primary">Button</a>
+        <div class="shadow p-3 mb-5 bg-white rounded">
+            <div class="card w-100">
+                <div class="card-body">
+                    <h5 class="card-title">Pending Maint Issues</h5>
+                    <p class="card-text">Pending issues with scheduled repair dates.</p>
+                    <a href="../maintRequests/maintPendingIssues.php" class="btn btn-primary">Button</a>
+                </div>
+            </div>
         </div>
-        </div>
-   
+        <div class="shadow p-3 mb-5 bg-white rounded">
+            <div class="card w-100">
+                <div class="card-body">
+                    <h5 class="card-title">Closed Maint Issues</h5>
+                    <p class="card-text">Closed issue history.</p>
+                    <a href="../maintRequests/maintClosedIssues.php" class="btn btn-primary">Button</a>
+                </div>
+            </div>
+        </div> 
     </div> 
-    <div class="container">
-        <?php 
-         ?>
-    </div> 
+    <!--==== FOOTER  Bootstrap 4 Class  Footer-Dark.css =======-->
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h2>Other Links</h2>
+                        <ul>
+                            <li><a href="#" alt="">link</a></li>
+                            <li><a href="#" alt="">link</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h2>About</h2>
+                        <ul>
+                            <li><a href="#">Purpose</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 item text">
+                        <h2>Main Site Title</h2>
+                        <p> sub paragraph</p>
+                        <img src="assets/img/logo.png" alt="" width="155">
+                    </div>
+                    <!-- Bottom Footer Icons -->
+                    <div class="col item social"> 
+                        <a href="#"><i class="fab fa-facebook-square"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </div>	
+                </div> <!-- end row -->
+                <p class="copyright">StinkyBoots Studio 2018</p>
+            </div>
+        </footer>
+    </div> <!-- end <div class="footer-dark">-->
+    <!--===== END FOOTER =============-->
 
 
 
