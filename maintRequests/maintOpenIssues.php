a5c9323bb6cd908691caec34b4f6ead3ab294291
stinkybootsllc <stinkybootsllc@gmail.com>
2019-01-27 17:25:51 -0500

    Oldest Issues Displayed First

--- maintRequests/maintOpenIssues.php
+++ maintRequests/maintOpenIssues.php
@@ -71,7 +71,7 @@
             JOIN Tenants tenantFname ON TenantMaintIssues.Tenant_FK = tenantFname.Tenant_ID
             JOIN Tenants tenantLname ON TenantMaintIssues.Tenant_FK = tenantLname.Tenant_ID
             JOIN Apartments tenantApt ON TenantMaintIssues.Tenant_Apt_FK = tenantApt.Apartment_ID
-            WHERE IssueStatus='open' ORDER BY IssueReportDate DESC";
+            WHERE IssueStatus='open' ORDER BY IssueReportDate ASC";
 
             $result = mysqli_query($conn, $sql);
 
