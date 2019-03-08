37e6beafe680e9c4b89d675fd63acb3f61bfc1d6
stinkybootsllc <stinkybootsllc@gmail.com>
2019-03-08 12:03:21 -0500

    user must be logged in to report - fix

--- tenants/confirmReport.php
+++ tenants/confirmReport.php
@@ -56,25 +56,29 @@
             $description = sanatizeData($_POST["IssueDescription"]);
             $tenantName = sanatizeData($_SESSION["TenantID"]); 
             $aptNumber = sanatizeData($_SESSION["TenantID"]);
-            // connect to database
-            require("../../Tenants_variables/tenant_dbconnect.php");
-            $clean_reportDate = mysqli_real_escape_string($conn,$reportDate);
-            $clean_description = mysqli_real_escape_string($conn,$description);
-            $clean_tenantName = mysqli_real_escape_string($conn,$tenantName);
-            $clean_aptNumber = mysqli_real_escape_string($conn,$aptNumber);
-            $sql = "INSERT INTO TenantMaintIssues (IssueReportDate, IssueStatus, IssueDescription, Tenant_FK, Tenant_Apt_FK)
-            VALUES ('$clean_reportDate', 'open','$clean_description', '$clean_tenantName','$clean_aptNumber')";
-            
-            if (mysqli_query($conn, $sql)) {
-                echo "New record created successfully";
-                // redirect back to dash
-                header('Refresh: 2; URL = tenantDash.php');
-            } else {
-                echo "Error: contact admin -- ec-69";
+            //if the tenant is logged in
+            if(isset($_SESSION['app_userEmail']) && isset($_SESSION['app_pass'])){
+                // connect to database
+                require("../../Tenants_variables/tenant_dbconnect.php");
+                $clean_reportDate = mysqli_real_escape_string($conn,$reportDate);
+                $clean_description = mysqli_real_escape_string($conn,$description);
+                $clean_tenantName = mysqli_real_escape_string($conn,$tenantName);
+                $clean_aptNumber = mysqli_real_escape_string($conn,$aptNumber);
+                $sql = "INSERT INTO TenantMaintIssues (IssueReportDate, IssueStatus, IssueDescription, Tenant_FK, Tenant_Apt_FK)
+                VALUES ('$clean_reportDate', 'open','$clean_description', '$clean_tenantName','$clean_aptNumber')";
+                
+                if (mysqli_query($conn, $sql)) {
+                    echo "New record created successfully";
+                    // redirect back to dash
+                    header('Refresh: 2; URL = tenantDash.php');
+                } else {
+                    echo "Error: contact admin -- ec-69";
+                    mysqli_close($conn);
+                }
                 mysqli_close($conn);
-            }
-            mysqli_close($conn);
-         
+            } else { 
+                echo "<h5>User Is Not Logged-In</h5>"; 
+            }// end if(isset($_SESSION['app_userEmail']) && isset($_SESSION['app_pass']))
         ?>
     </div>
     <!-- Optional JavaScript -->
