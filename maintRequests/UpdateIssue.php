44d368412012072c955724182c22487741e46885
stinkyBootsLLC <stinkybootsllc@gmail.com>
2019-11-03 19:08:11 -0500

    added responsive control to the form

--- maintRequests/UpdateIssue.php
+++ maintRequests/UpdateIssue.php
@@ -54,11 +54,12 @@
             </div>
         </nav>
         <!-- Top Nav Bar -->
-
+        <div class="top-spacing"></div>
         <div class="container-fluid">
             <h3>Tenant Portal Application</h3>
             <p>Update Maintenace issue</p>
         </div>
+		
     
         <div class="container">
     
@@ -110,54 +111,57 @@
                     }// end while($row = mysqli_fetch_assoc($result))
                     // create the form
                     echo "<form action='confirmUpdate.php' method='POST'>";
-                    echo "<div class='form-group'>";
-                    echo "<fieldset>";
+                    echo "<fieldset class='form-group'>";
                     echo "<legend>Maint Issue:</legend>";
-                    echo "Issue ID:<input type='text' id='issueID' name='id' value=".$id." readonly>";
-                    echo "Reported:<input type='date'  id='IssueReportDate' name='IssueReportDate' value=".$IssueReportDate." readonly>";
-                    echo "Priority: <select name='IssuePriority'>";
+                    echo "<label for='issue id'>Issue ID:</label>";
+                    echo "<input class='form-control' type='text' id='issueID' name='id' value=".$id." readonly>";
+                    echo "<label for='Reported'>Reported:</label><input class='form-control' type='date' id='IssueReportDate' name='IssueReportDate' value=".$IssueReportDate." readonly>";
+                    echo "<label for='Priority'>Priority:</label><select class='form-control' name='IssuePriority'>";
                     echo "<option value=".$IssuePriority.">".$IssuePriority."</option>";
                     echo "<option value='High'>High</option>";
                     echo "<option value='Medium'>Medium</option>";
                     echo "<option value='Low'>Low</option>";
                     echo "</select>"; 
-                    echo "Status: <select name='IssueStatus'>";
+                    echo "<label for='Status'>Status:</label><select class='form-control' name='IssueStatus'>";
                     echo "<option value=".$IssueStatus.">".$IssueStatus."</option>";
-                
                     echo "<option value='pending'>pending</option>";
                     echo "<option value='closed'>closed</option>";
                     echo "</select><br>"; 
                     echo "</fieldset>";
-                    echo "</div>";
+
+
+
+
+                    //echo "</div>";
                     // description field
-                    echo "<div class='form-group'>";
+                    echo "<fieldset class='form-group'>";
                     echo "<label for='Description'>Description:</label>";
                     echo "<textarea  class='form-control' name='IssueDescription'rows='2' cols='33' id='Description' readonly>".$IssueDescription."</textarea>";
-                    echo "</div> ";
+                    echo "</fieldset> ";
                     // solution field
-                    echo "<div class='form-group'>";
+                    echo "<fieldset class='form-group'>";
                     echo "<label for='Solution'>Solution:</label>";
                     echo "<textarea  class='form-control' name='IssueSolution'rows='2' cols='33' id='Solution'>".$IssueSolution."</textarea><br>";
-                    echo "</div> ";
+                    echo "</fieldset> ";
                     // update info fieldset
-                    echo "<div class='form-group'>";
+                    echo "<fieldset class='form-group'>";
                     echo "<fieldset>";
                     echo "<legend>Repair Info:</legend>";
-                    echo "Scheduled: <input type='date' name='ScheduledDate' value=".$ScheduledDate.">";
-                    echo "Repaired: <input type='date' name='IssueRepairDate' value=".$IssueRepairDate.">";
-                    echo "Repair Price: <input type='text' name='IssueRepairPrice' value=".$IssueRepairPrice."><br>";
+                    echo "<label for='Scheduled'>Scheduled:</label> <input class='form-control' type='date' name='ScheduledDate' value=".$ScheduledDate.">";
+                    echo "<label for='Repaired'>Repaired:</label> <input class='form-control' type='date' name='IssueRepairDate' value=".$IssueRepairDate.">";
+                    echo "<label for='Repair Price'> Repair Price:</label> <input class='form-control' type='text' name='IssueRepairPrice' value=".$IssueRepairPrice."><br>";
+                  
                     echo "</fieldset>";
-                    echo "</div>";
                     // tenant name field
-                    echo "<div class='form-group'>";
+                    echo "<fieldset class='form-group'>";
                     echo "<label for='Tenant Name'>Tenant Name:</label>";
-                    echo "<input type='text' class='form-control' name='tenantName' id='Tenant Name' value=".$tenantName." readonly><br>";
-                    echo "</div>";
+                    echo "<input class='form-control' type='text' name='tenantName' id='Tenant Name' value=".$tenantName." readonly><br>";
+                    echo "</fieldset>";
                     // tenant apt number field
-                    echo "<div class='form-group'>";
+                    echo "<fieldset class='form-group'>";
                     echo "<label for='TenantApt'>Tenant Apt Number:</label>";
-                    echo "<input type='text'  class='form-control' name='aptNumber' id='TenantApt' value=".$aptNumber." readonly><br>";
-                    echo "</div>";
+                    echo "<input class='form-control' type='text' name='aptNumber' id='TenantApt' value=".$aptNumber." readonly><br>";
+                    echo "</fieldset>";
                     // submit button
                     echo "<input class='btn btn-primary' type='submit' value='UPDATE'>";
                     echo "</form>";
